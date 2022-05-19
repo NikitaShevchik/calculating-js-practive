0fb1afe4889f07055f2bd90b62ebd5525f995101
NikitaShevchik <shevchik.nikita@gmail.com>
2022-05-19 15:50:44 +0300

    circle

--- js/scrypt.js
+++ js/scrypt.js
@@ -9,8 +9,8 @@ const resultAreaSquare = document.querySelector(".square__area");
 
 buttonGetResultSquare.addEventListener("click", function (e) {
     var getSide = getFromInputSquare.value;
-    resultPerimetrSquare.innerHTML = `${getSide * 4}`;
-    resultAreaSquare.innerHTML = `${getSide * getSide}`;
+    resultPerimetrSquare.innerHTML = `${getSide * 4} см`;
+    resultAreaSquare.innerHTML = `${getSide * getSide} см`;
 });
 
 /*--------RECTANGLE--------*/
@@ -23,6 +23,35 @@ const resultAreaRectangle = document.querySelector(".rectangle__area");
 buttonGetResultRectangle.addEventListener("click", function (e) {
     var getSideFirst = getFromInputRectangleFirst.value;
     var getSideSecond = getFromInputRectangleSecond.value;
-    resultPerimetrRectangle.innerHTML = `${(getSideFirst * 2) + (getSideSecond * 2)}`;
-    resultAreaRectangle.innerHTML = `${getSideFirst * getSideSecond}`;
+    resultPerimetrRectangle.innerHTML = `${(getSideFirst * 2) + (getSideSecond * 2)} см`;
+    resultAreaRectangle.innerHTML = `${getSideFirst * getSideSecond} см`;
+});
+
+/*--------CIRCLE--------*/
+const buttonGetResultCircle = document.querySelector(".circle__button");
+const getFromInputCircle = document.querySelector(".circle__number");
+const resultСircumferenceCircle = document.querySelector(".circle__circumference");
+const resultAreaCircle = document.querySelector(".circle__area");
+
+buttonGetResultCircle.addEventListener("click", function (e) {
+    var gerRadius = getFromInputCircle.value;
+    var pi = 3.14;
+    resultСircumferenceCircle.innerHTML = `${2 * pi * gerRadius} см`;
+    resultAreaCircle.innerHTML = `${pi * (gerRadius * gerRadius)} см`;
+})
+
+/*--------TRIANGLE--------*/
+const buttonGetResultTriangle = document.querySelector(".triangle__button");
+const getFromInputTriangleFirst = document.querySelector(".triangle__number_1");
+const getFromInputTriangleSecond = document.querySelector(".triangle__number_2");
+const getFromInputTriangleThird = document.querySelector(".triangle__number_3");
+const resultAreaTriangle = document.querySelector(".triangle__area");
+
+buttonGetResultTriangle.addEventListener("click", function (e) {
+    var getSideFirst = Number(getFromInputTriangleFirst.value);
+    var getSideSecond = Number(getFromInputTriangleSecond.value);
+    var getSideThird = Number(getFromInputTriangleThird.value);
+    var halfPerimetrTriangle = (getSideFirst + getSideSecond + getSideThird) / 2;
+    var areaTriangle = Math.sqrt(halfPerimetrTriangle * (halfPerimetrTriangle - getSideFirst) * (halfPerimetrTriangle - getSideSecond) * (halfPerimetrTriangle - getSideThird));
+    resultAreaTriangle.innerHTML = `${areaTriangle.toFixed(2)} см`;
 });
